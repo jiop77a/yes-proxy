bd8bc3f8e551c7d6d4b41b5abe5048117b88bbc2
Alex E Scott <aescott@outlook.com>
2017-10-04 11:21:00 -0700

    second commit

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 // Listen on a specific host via the HOST environment variable
-var host = process.env.HOST || '127.0.0.1';
+var host = process.env.PORT ? '0.0.0.0' : '127.0.0.1';
 // Listen on a specific port via the PORT environment variable
 var port = process.env.PORT || 8080;
 
@@ -7,7 +7,19 @@ var cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
     originWhitelist: [], // Allow all origins
     requireHeader: ['origin', 'x-requested-with'],
-    removeHeaders: ['cookie', 'cookie2']
+    removeHeaders: [
+      'cookie',
+      'cookie2',
+      // Strip Heroku-specific headers
+      'x-heroku-queue-wait-time',
+      'x-heroku-queue-depth',
+      'x-heroku-dynos-in-use',
+      'x-request-start',
+    ],
+    httpProxyOptions: {
+      // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
+      xfwd: false,
+    },
 }).listen(port, host, function() {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
